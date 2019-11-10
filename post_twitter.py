c07a53d0415e2e22d23bf006715c5208f39c9df8
Yunzhi Gao <gaoyunzhi@gmail.com>
2019-11-10 16:40:57 +0200

    fix photo upload

--- post_twitter.py
+++ post_twitter.py
@@ -6,30 +6,35 @@ from telegram.ext import Updater, MessageHandler, Filters
 import traceback as tb
 import json
 import tweepy
+import yaml
 
 with open('CREDENTIALS') as f:
 	CREDENTIALS = json.load(f)
 
 with open('KEYS') as f:
-	KEYS = set(json.load(f))
+	KEYS = set(yaml.load(f, Loader=yaml.FullLoader))
 
 def matchKey(t):
-	for k in keys:
+	print(t)
+	if not t:
+		return False
+	for k in KEYS:
 		if k in t:
 			return True
 	return False
 
 def manage(update, context):
 	try:
-		msg = update.message 
+		msg = update.effective_message 
 		if (not msg) or msg.media_group_id or (not update.effective_chat):
 			return
 		if (not matchKey(msg.text)) and \
-			(not matchKey(update.effective_chat.first_name)):
+			(not matchKey(update.effective_chat.title)):
 			return
-		print(msg.photo)
 		if msg.photo:
-			return api.update_with_media(msg.photo[-1])
+			filename = 'tmp.' + msg.photo[-1].get_file().file_path.strip().split('/')[-1]
+			msg.photo[-1].get_file().download(filename)
+			return api.update_with_media(filename)
 		api.update_status(msg.text)
 	except Exception as e:
 		print(e)
@@ -43,3 +48,6 @@ updater = Updater(CREDENTIALS['bot_token'], use_context=True)
 dp = updater.dispatcher
 
 dp.add_handler(MessageHandler(Filters.update.channel_posts, manage))
+
+updater.start_polling()
+updater.idle()
