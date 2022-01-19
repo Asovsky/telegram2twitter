106a7d0152106f165938c161615cced973e04cb1
Yunzhi Gao <gaoyunzhi@gmail.com>
2022-01-18 20:51:47 -0800

    commit

--- post_twitter.py
+++ post_twitter.py
@@ -61,7 +61,7 @@ def getLinkReplace(url, album, item, all_text):
             return ''
 
     title = export_to_telegraph.getTitle(url)
-    if title in ['No Title', '[no-title]'] or matchKey(url, ['facebook', 'twitter', 'tumblr']):
+    if title in ['No Title', '[no-title]'] or matchKey(url, ['facebook', 'twitter', 'tumblr', 'reddit', 'instagram']):
         return '\n\n' + url
     return '\n\n【%s】 %s' % (title, url)
 
