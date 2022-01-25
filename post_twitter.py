010ceedfc4fe1d6cce060042b20ccb643ac32b88
Yunzhi Gao <gaoyunzhi@gmail.com>
2022-01-25 07:32:03 -0800

    commit

--- post_twitter.py
+++ post_twitter.py
@@ -59,6 +59,9 @@ def getLinkReplace(url, album, item, all_text):
             url = soup.find('address').find('a')['href']
         except:
             return ''
+    
+    if matchKey(all_text, ['： ' + item.text.strip(), ': ' + item.text.strip()]):
+        return url
 
     title = export_to_telegraph.getTitle(url)
     if title in ['No Title', '[no-title]'] or matchKey(url, ['facebook', 'twitter', 'tumblr', 'reddit', 'instagram']):
@@ -213,11 +216,11 @@ async def runImp():
                 continue
             status_text = getText(album, post) or album.url
             if credential['channels'][channel].get('cut_text'):
-                print('before cut', status_text)
+                # print('before cut', status_text)
                 status_text = cutText(status_text, 
                     credential['channels'][channel].get('cut_text_retain_link'),
                     credential['channels'][channel].get('cut_text_splitter'))
-                print('after cut', status_text)
+                # print('after cut', status_text)
             if len(status_text) > 280: 
                 continue
             existing.update(album.url, -1) # place holder
