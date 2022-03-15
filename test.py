07bc28c186dddbf597c6e46ddd94a4ff2adcf200
Yunzhi Gao <gaoyunzhi@gmail.com>
2022-03-15 08:48:52 -0700

    commit

--- test.py
+++ test.py
@@ -1,2 +1,16 @@
-for i in range(4000, 4176):
-	print('https://t.me/translate_img/' + str(i))
+import tweepy
+import yaml
+
+with open('credential') as f:
+    credential = yaml.load(f, Loader=yaml.FullLoader)
+
+def addAccount():
+    auth = tweepy.OAuthHandler(credential['twitter_consumer_key'], credential['twitter_consumer_secret'])
+    auth.get_authorization_url()
+    verifier = ''
+    auth.get_access_token(verifier)
+
+if __name__ == '__main__':
+    ...
+    # addAccount()
+
